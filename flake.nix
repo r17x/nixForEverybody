eefa1d4b7ab6bf410cea8a6c3d39c3004e93b8f3
r17x <ri7nz@evilfactory.id>
2024-02-18 22:56:27 +0700

    feat(packages): very simple binary packages with shellscript

--- flake.nix
+++ flake.nix
@@ -7,21 +7,38 @@
   outputs = inputs: inputs.u.lib.eachDefaultSystem (system:
     let
       pkgs = inputs.nixpkgs.legacyPackages.${system};
+
+      ilham = pkgs.writeScriptBin "ilham" ''
+        echo "from ilham di jakarta"
+      '';
     in
     {
+      # nix build .#ilham -- build
+      # nix run .#ilham -- jalankan
+      packages = {
+        # same-thing: ilham=ilham;
+        inherit ilham;
+        # same-thing: git=pkgs.git; 
+        # inherit (pkgs) git;
+      };
       devShells = {
+        # nix develop 
+        # empty
         default = pkgs.mkShell {
           buildInputs = [ ];
         };
         # nix develop .#python
+        # same-thing: nix-shell -p python3
         python = pkgs.mkShell {
           buildInputs = [ pkgs.python3 ];
         };
         # nix develop .#nodejs
+        # same-thing: nix-shell -p nodejs
         nodejs = pkgs.mkShell {
           buildInputs = [ pkgs.nodejs pkgs.nodePackages.yarn pkgs.nodePackages.pnpm ];
         };
-
+        # nix develop .#go
+        # same-thing: nix-shell -p go
         go = pkgs.mkShell {
           buildInputs = [ pkgs.go ];
         };
